b91613bcca87a3124f7351fe6489d9f527d5188f
AlexisBrouardYnov <67193389+AlexisBrouardYnov@users.noreply.github.com>
2021-04-03 02:14:15 +0200

    Ajout delai entre tirs

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,19 +9,29 @@ public class Weapon : MonoBehaviour
 
     public Animator animator;
 
+    private bool isShooting = true;
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") && isShooting==true)
         {
             animator.SetTrigger("Shoot");
             Shoot();
+            isShooting = false;
+            StartCoroutine(shoot_delayed(0.5f));
         }
     }
 
     void Shoot()
     {
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    IEnumerator shoot_delayed(float temps)
+    {
+        yield return new WaitForSeconds(temps);
+        isShooting = true;
 
     }
 }
